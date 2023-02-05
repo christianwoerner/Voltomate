357c0bcdbe7e873b8e01050bbfe198fdbb3fbdfb
christianwoerner <56825392+christianwoerner@users.noreply.github.com>
2023-02-05 01:36:45 +0100

    final pipeline works

--- import requests.py
+++ import requests.py
@@ -6,10 +6,17 @@ import urllib
 import openai
 import time
 import re
+from moviepy.editor import *
+from moviepy.config import change_settings
+import time 
+from PIL import Image
+
+change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.0-Q16-HDRI\\magick.exe"})
 
 class reddit_grabber():
     def __init__(self):
         self.get_auth_token()
+        self.linked_media_url = None
         
     def get_auth_token(self):
         CLIENT_AUTH = requests.auth.HTTPBasicAuth('r5CgWuFB6P7XcZoiPZgLGA', 'UpY25X1_lKRqY9I9rJDkb-FCpt1mGw')
@@ -28,55 +35,160 @@ class reddit_grabber():
         self.result = res.json()
 
     def save_content_as_mp3_file(self,title):
+
         self.title = title 
-        print(title)       
+        self.title = self.title.replace("TIL", "Today I learned")
+
+        print(self.title)       
+
         engine = pyttsx3.init()
+        engine.setProperty("rate", 145)
+
         voices = engine.getProperty('voices')       #getting details of current voice
         engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
-        engine.save_to_file(title, 'test2.mp3')
+        self.mp3name = str(time.strftime("%Y%m%d_%H%M%S"))+".mp3"
+        engine.save_to_file(self.title, self.mp3name)
         engine.runAndWait()
 
     def get_image_from_dalle(self):
-        self.a = False
-        if self.a == True:
-            #openai.api_key = "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"
-            openai.api_key = "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"
-            openai.api_key = "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"
-            openai.organization = "org-9kOJ7yh966xJKuo7UXKVfVzn"
-            openai.Model.list()
+        #openai.api_key = "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"
+        openai.api_key = "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"
+        openai.api_key = "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"
+        openai.api_key = "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"
+        openai.organization = "org-9kOJ7yh966xJKuo7UXKVfVzn"
+        openai.Model.list()
 
-            regex_pattern = r"[,.]"
-            self.prompt_title_part= re.split(regex_pattern, self.title)[0]
-            print('requested for: '+str(self.prompt_title_part))
+        regex_pattern = r"[,.]"
+        self.prompt_title_part= re.split(regex_pattern, self.title)[0]
+        print('requested for: '+str(self.prompt_title_part))
 
-            response = openai.Image.create(
-            prompt=self.prompt_title_part,
-            n=1,
-            size="1024x1024"
+        response = openai.Image.create(
+        prompt=self.prompt_title_part,
+        n=1,
+        size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+        self.image_name = str(time.strftime("%Y%m%d_%H%M%S"))+".png"
+        urllib.request.urlretrieve(image_url, self.image_name)
+
+    def create_video(self):
+        # Load the audio file using moviepy
+        print("Extract voiceover and get duration...")
+        audio_clip = AudioFileClip(self.mp3name)
+        audio_duration = audio_clip.duration
+
+
+        img = Image.open(self.image_name)
+        img_cropped = img.crop((224, 0, 800, 1024))
+        self.image_name_cropped = self.image_name+"cropped.png"
+        img_cropped.save(self.image_name_cropped, format=img.format)
+
+        # Load the image file using moviepy
+        print("Extract Image Clip and Set Duration...")
+        image_clip = ImageClip(self.image_name_cropped).set_duration(audio_duration)
+
+        # # Use moviepy to create a text clip from the text
+        # print("Customize The Text Clip...")
+        # text_clip = TextClip("TIL Bats and dolphins evolved echolocation in the same way (down to the molecular level). An analysis revealed that 200 genes had independently changed in the same way", fontsize=50, color="white")
+        # print("here")
+
+        #text_clip = text_clip.set_pos('center').set_duration(audio_duration)
+        # Use moviepy to create a final video by concatenating
+        # the audio, image, and text clips
+        print("Concatenate Audio, Image, Text to Create Final Clip...")
+        clip = image_clip.set_audio(audio_clip)
+
+        screensize = (576,1024)
+
+        credits = (TextClip(self.title, color='white',stroke_color= 'black',
+                font="Berlin-Sans-FB-Bold", kerning=-2, interline=-1, size = 
+        screensize, method='caption')
+            .set_duration(audio_duration)
+            .set_start(0)
             )
-            image_url = response['data'][0]['url']
-            self.image_name = str(time.strftime("%Y%m%d_%H%M%S"))+".png"
-            urllib.request.urlretrieve(image_url, self.image_name)
+
+
+        video = CompositeVideoClip([clip, credits])
+        # video = CompositeVideoClip([clip, text_clip.set_pos(('center', 'bottom'))])
 
 
 
+        # Save the final video to a file
+        self.video_name = str(time.strftime("%Y%m%d_%H%M%S"))+"vid.mp4"
+        with open(self.video_name+'.txt', 'w') as f:
+            f.write(reddit_content.linked_media_url)
+        video = video.write_videofile(self.video_name, fps=24)
+        print(f"The Video Has Been Created Successfully!")
 
+#%%
+#
+# 
+# 
 reddit_content = reddit_grabber()
 print(reddit_content.token)
 reddit_content.get_content_for_subreddit('TodayILearned')
 
-reddit_content.save_content_as_mp3_file(reddit_content.result['data']['children'][12]['data']['title'])
+k = 7
+reddit_content.save_content_as_mp3_file(reddit_content.result['data']['children'][k]['data']['title'])
+reddit_content.linked_media_url = reddit_content.result['data']['children'][k]['data']['url_overridden_by_dest']
 
-# reddit_content.a = True
 reddit_content.get_image_from_dalle()
+#reddit_content.image_name = '20230205_010857.png'
+
+reddit_content.create_video()
+
+print(reddit_content.video_name)
+
+#%%
+#
+#
+reddit_content = reddit_grabber()
+print(reddit_content.token)
+reddit_content.get_content_for_subreddit('TodayILearned')
+
+for i in range(len(reddit_content.result['data']['children'])):
+    print(i)
+    print(reddit_content.result['data']['children'][i]['data']['title'])
+#%%
+k = 7
+reddit_content.save_content_as_mp3_file(reddit_content.result['data']['children'][k]['data']['title'])
+reddit_content.linked_media_url = reddit_content.result['data']['children'][k]['data']['url_overridden_by_dest'])
+
+reddit_content.get_image_from_dalle()
+#reddit_content.image_name = '20230205_010857.png'
+
+reddit_content.create_video()
+
+print(reddit_content.video_name)
+
+
+
+
+#%%
+reddit_content = reddit_grabber()
+
+reddit_content.save_content_as_mp3_file("This is a test. Let's see how fast you talk now")
 
 
 #%%
 
+CLIENT_AUTH = requests.auth.HTTPBasicAuth('r5CgWuFB6P7XcZoiPZgLGA', 'UpY25X1_lKRqY9I9rJDkb-FCpt1mGw')
+POST_DATA = {"grant_type": "password", "username": "voltomate", "password": "Reddit003465!"}
+HEADERS = {"User-Agent": "CW/0.1 by CW"}
+response = requests.post("https://www.reddit.com/api/v1/access_token", auth=CLIENT_AUTH, data=POST_DATA, headers=HEADERS)
+response.json()
+token = response.json()['access_token']
 
+HEADERS = {**{"User-Agent": "CW/0.1 by CW"}, **{'Authorization': f"bearer {token}"}}
+# while the token is valid (~2 hours) we just add HEADERS=HEADERS to our requests
+requests.get('https://oauth.reddit.com/api/v1/me', headers=HEADERS)
+res = requests.get(f"https://oauth.reddit.com/r/TodayILearned/top",
+                headers=HEADERS)
+result = res.json()
+#%%
 
-
-
+print(result['data']['children'][13])
+print(result['data']['children'][13]['data']['url_overridden_by_dest'])
 
 
 
@@ -97,60 +209,8 @@ reddit_content.get_image_from_dalle()
 
 # %%
 
-from moviepy.editor import *
-from moviepy.config import change_settings
-import time 
-from PIL import Image
-
-change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.0-Q16-HDRI\\magick.exe"})
-
-# Load the audio file using moviepy
-print("Extract voiceover and get duration...")
-audio_clip = AudioFileClip(f"test2.mp3")
-audio_duration = audio_clip.duration
-
-filename = "20230204_235944"
-img = Image.open(filename+".png")
-img_cropped = img.crop((224, 0, 800, 1024))
-img_cropped.save(filename+"cropped.png", format=img.format)
-
-# Load the image file using moviepy
-print("Extract Image Clip and Set Duration...")
-image_clip = ImageClip(filename+"cropped.png").set_duration(audio_duration)
-# 576 1024
 
 
-# Use moviepy to create a text clip from the text
-print("Customize The Text Clip...")
-text_clip = TextClip("TIL Bats and dolphins evolved echolocation in the same way (down to the molecular level). An analysis revealed that 200 genes had independently changed in the same way", fontsize=50, color="white")
-print("here")
-
-text_clip = text_clip.set_pos('center').set_duration(audio_duration)
-# Use moviepy to create a final video by concatenating
-# the audio, image, and text clips
-print("Concatenate Audio, Image, Text to Create Final Clip...")
-clip = image_clip.set_audio(audio_clip)
-
-
-text = "TIL Bats and dolphins evolved echolocation in the same way (down to the molecular level). An analysis revealed that 200 genes had independently changed in the same way"
-screensize = (576,1024)
-
-credits = (TextClip(text, color='white',stroke_color= 'black',
-        font="Berlin-Sans-FB-Bold", kerning=-2, interline=-1, size = 
-screensize, method='caption')
-      .set_duration(audio_duration)
-      .set_start(0)
-      )
-
-
-
-video = CompositeVideoClip([clip, credits])
-# video = CompositeVideoClip([clip, text_clip.set_pos(('center', 'bottom'))])
-
-
-# Save the final video to a file
-video = video.write_videofile(str(time.strftime("%Y%m%d_%H%M%S"))+"vid.mp4", fps=24)
-print(f"The Video Has Been Created Successfully!")
 
 #%%
 # from PIL import Image
